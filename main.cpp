24c1d3e1d83aef347ab4edb5c2d16fb9e344a055
Narotan <danilsild2006@gmail.com>
2025-03-03 00:44:38 +0700

    финальное воссоединение

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <cctype>
 
 using namespace std;
 
@@ -17,15 +19,67 @@ bool checkIfPalindrome(string& inString) {
     return true;
 }
 
-int main() {
-    cout << "введите строку: ";
-    string inString;
-    getline(cin, inString);
-    if (checkIfPalindrome(inString)) {
-        cout << "вы ввели палиндром." << endl;
+
+string toUpper(const string &s) {
+    string result = s;
+    for (char &c : result) {
+        c = toupper(c);
     }
-    else {
-        cout << "вы ввели не палиндром" << endl;
+    return result;
+}
+
+string toLower(const string &s) {
+    string result = s;
+    for (char &c : result) {
+        c = tolower(c);
+    }
+    return result;
+}
+
+int main() {
+
+    while (true) {
+        cout << "\nВыберите номер фрагмента:\n"
+             << "1. Приведение текста к верхнему и нижнему регистрам (только английские) \n"
+             << "2. Палиндром или нет?\n"
+             << "Номер? (0 для выхода): ";
+        int choice;
+        cin >> choice;
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        switch (choice) {
+            case 1: {
+                string s;
+                cout << "Enter a string: ";
+                getline(cin, s);
+                cout << "Uppercase: " << toUpper(s) << endl;
+                cout << "Lowercase: " << toLower(s) << endl;
+                break;
+            }
+
+            case 2: {
+                cout << "введите строку: ";
+                string inString;
+                getline(cin, inString);
+                if (checkIfPalindrome(inString)) {
+                    cout << "вы ввели палиндром." << endl;
+                }
+                else {
+                    cout << "вы ввели не палиндром" << endl;
+                }
+                break;
+            }
+            case 0: {
+                cout << "Выход из программы." << endl;
+                return 0;
+            }
+
+            default: {
+                cout << "Неверный выбор. Попробуйте снова." << endl;
+                break;
+            }
+        }
     }
     return 0;
 }
