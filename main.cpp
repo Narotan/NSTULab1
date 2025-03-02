37779d18efb814f6dd43146fc7271ac5630677f9
shuballity <nikitadmitriev9897@gmail.com>
2025-03-03 00:13:12 +0700

    adding cpp script

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+bool checkIfPalindrome(string& inString) {
+    int leftPointer = 0;
+    int rightPointer = inString.length() - 1;
+    while (leftPointer < rightPointer) {
+        if (inString[leftPointer] != inString[rightPointer]) {
+            return false;
+        }
+        else {
+            leftPointer++;
+            rightPointer--;
+        }
+    }
+    return true;
+}
+
+int main() {
+    cout << "введите строку: ";
+    string inString;
+    getline(cin, inString);
+    if (checkIfPalindrome(inString)) {
+        cout << "вы ввели палиндром." << endl;
+    }
+    else {
+        cout << "вы ввели не палиндром" << endl;
+    }
     return 0;
 }
